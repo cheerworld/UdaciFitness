0a87dc808bd38dd117a9bcb149d11a35b75ecff0
Yuguo Zhao <cheerzhao77@gmail.com>
2021-03-16 10:37:12 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  componentDidMount () {
+    console.log("before")
+    debugger
+    console.log("after")
+  }
+  render () {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Cheer. Hello everyone!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
