455e09f2068f4525aa76e0800508e1a6f45aaf62
Yuguo Zhao <cheerzhao77@gmail.com>
2021-03-21 10:48:29 -0500

    feat: add Slider to App, not working

--- App.js
+++ App.js
@@ -4,16 +4,24 @@ import {
   View,
   Text,
   StyleSheet,
+  Slider,
 } from "react-native";
 import AddEntry from "./components/AddEntry";
 
 export default class App extends React.Component {
-  
+  state = {
+    value: 0
+  }
 
   render() {
     return (
       <View>
         <AddEntry />
+        <Slider
+          value={this.state.value}
+          onValueChange={(value) => this.setState(() => ({ value }))}
+        />
+        <Text>Value: {this.state.value}</Text>
       </View>
     );
   }
