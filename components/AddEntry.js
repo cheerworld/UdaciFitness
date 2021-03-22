dfb8874c62ee9eb02f63989c23ed3d9073c64e99
Yuguo Zhao <cheerzhao77@gmail.com>
2021-03-21 19:09:04 -0500

    feat: add reset TextButton

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,14 +4,15 @@ import { getMetricMetaInfo, timeToString } from "../utils/helpers";
 import UdaciSlider from "./UdaciSlider";
 import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
+import { Ionicons } from "@expo/vector-icons";
+import TextButton from "./TextButton";
 
-function SubmitBtn ({ onPress }) {
+function SubmitBtn({ onPress }) {
   return (
-    <TouchableOpacity
-      onPress={onPress}>
-        <Text>SUBMIT</Text>
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
     </TouchableOpacity>
-  )
+  );
 }
 
 export default class AddEntry extends Component {
@@ -65,21 +66,41 @@ export default class AddEntry extends Component {
       swim: 0,
       sleep: 0,
       eat: 0,
-    }))
+    }));
 
     // Navigate to home
 
     // Save to "DB"
 
     //Clear local notification
-  }
+  };
+
+  reset = () => {
+    const key = timeToString();
+
+    //Update Redux
+
+    //Route to home
+
+    //Update "DB"
+  };
 
   render() {
     const metaInfo = getMetricMetaInfo();
 
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons name="ios-happy-outline" size={100} />
+          <Text>You already logged your information for today</Text>
+          <TextButton onPress={this.reset}>Reset</TextButton>
+        </View>
+      );
+    }
+
     return (
       <View>
-        <DateHeader date={(new Date()).toLocaleDateString()}/>
+        <DateHeader date={new Date().toLocaleDateString()} />
         <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key];
@@ -105,7 +126,7 @@ export default class AddEntry extends Component {
             </View>
           );
         })}
-        <SubmitBtn onPress={this.submit}/>
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
