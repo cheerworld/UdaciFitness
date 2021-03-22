017d231f5b885f1309f0cfe1b83f903a6d15f924
Yuguo Zhao <cheerzhao77@gmail.com>
2021-03-21 21:31:31 -0500

    feat: try to use SectionList

--- App.js
+++ App.js
@@ -1,6 +1,34 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, SectionList } from "react-native";
 import AddEntry from "./components/AddEntry";
+
+const DATA = [
+  {
+    pop: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"]
+  },
+  {
+    pop: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+  },
+  {
+    pop: "Drinks",
+    data: ["Water", "Coke", "Beer"]
+  },
+  {
+    pop: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"]
+  }
+];
+
+const Item = ({item}) => {
+  console.log(item);
+  return (
+    <View>
+      <Text style={styles.title}>{item}</Text>
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   state = {
@@ -11,6 +39,13 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <AddEntry />
+        <SectionList
+          sections={DATA}
+          renderItem={({item})=> <Item item={item}/>}
+          renderSectionHeader={({section: {pop}})=>(
+            <Text style={styles.header}>{pop}</Text>
+          )}
+        />
       </View>
     );
   }
@@ -24,4 +59,16 @@ const styles = StyleSheet.create({
     alignItems: "stretch",
     justifyContent: "center",
   },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#fff"
+  },
+  title: {
+    fontSize: 24
+  }
 });
